48c878d1f2707c55c47be414af091e7c29931467
ccatron88 <christophercatron@outlook.com>
2022-02-14 14:13:07 -0600

    Changes and deep in-progress commit

---
+++ StorefrontApp/Models/UserDataForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace StorefrontApp.Models
+
+{
+	public class UserDataForm
+	{
+		[Required]
+		public string? FirstName { get; set; }
+
+		[Required]
+		public string? LastName { get; set; }
+
+		[Required]
+		public string? EmailAddress { get; set; }
+
+		public UserDataForm()
+		{
+		}
+	}
+}
+
